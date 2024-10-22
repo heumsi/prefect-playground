8737b212b6198b577935411744957287228cde80
heumsi <heumsi@gmail.com>
2024-10-22 18:22:02 +0900

    Modify flow.py

--- flow.py
+++ flow.py
@@ -1,7 +1,7 @@
 from prefect import flow
 
 
-@flow
+@flow(log_prints=True)
 def my_first_flow(name: str = "world"):
-    print(f"Hello {name} from Prefect! 🤗")
+    print(f"Hello {name} from Prefect! 🤗 This is modified!")
 
